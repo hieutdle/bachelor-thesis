a7195621c3e0e407f101631e881d984e56c12d35
Hieu Le Dinh Trung <hieu.bachelor.thesis@gmail.com>
2022-04-05 10:10:55 +0000

    update

--- models/core/procedure_occurrence.sql
+++ models/core/procedure_occurrence.sql
@@ -3,7 +3,7 @@
 SELECT
   row_number()over(order by p.person_id) as procedure_occurrence_id,
   p.person_id,
-  coalesce(srctostdvm.target_concept_id, 0) as procedure_concept_id,
+  coalesce(srctostdvm.target_concept_id, CAST(0 AS STRING)) as procedure_concept_id,
   CAST(pr.date as TIMESTAMP) as procedure_date,
   CAST(pr.date as TIMESTAMP) as procedure_datetime,
   38000275 as procedure_type_concept_id,
@@ -13,17 +13,17 @@ SELECT
   fv.visit_occurrence_id_new AS visit_occurrence_id,
   fv.visit_occurrence_id_new + 1000000 as visit_detail_id,
   pr.code as procedure_source_value,
-  coalesce(srctosrcvm.source_concept_id,0) as procedure_source_concept_id,
+  coalesce(srctosrcvm.source_concept_id,CAST(0 AS STRING)) as procedure_source_concept_id,
   NULL as modifier_source_value
 from {{source('raw_data_warehouse','procedures')}} pr
 inner join {{ref('source_to_standard_vocab_map')}} srctostdvm
-  on srctostdvm.source_code             = pr.code
+  on srctostdvm.source_code             = CAST(pr.code AS STRING)
  and srctostdvm.target_domain_id        = 'Procedure'
  and srctostdvm.source_vocabulary_id    = 'SNOMED'
  and srctostdvm.target_standard_concept = 'S'
  and (srctostdvm.target_invalid_reason IS NULL OR srctostdvm.target_invalid_reason = '')
 left join {{ref('source_to_source_vocab_map')}} srctosrcvm
-  on srctosrcvm.source_code             = pr.code
+  on srctosrcvm.source_code             = CAST(pr.code AS STRING)
  and srctosrcvm.source_vocabulary_id    = 'SNOMED'
 left join {{ref('final_visit_ids')}} fv
   on fv.encounter_id = pr.encounter
